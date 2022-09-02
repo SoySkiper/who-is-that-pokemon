4e2a04042bb3581beb6d87825e80aa8810d37810
Cristian Primero <soyskiper@gmail.com>
2022-09-01 19:24:18 -0500

    Aplicacion terminada, lista para producción

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,39 +1,50 @@
 <template>
-  <div class="options-container">
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
-  </div>
+	<div class="options-container">
+		<ul>
+			<!-- $emit se usa para Emitir evento a PokemonPage -->
+			<li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selection', pokemon.id)">
+				{{ pokemon.name }}
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 export default {
-
+	props: {
+		pokemons: {
+			type: Array,
+			require: true
+		}
+	},
+	methods: {
+		hola(){
+			//this.$emit()
+		}
+	}
 }
 </script>
 
 <style scoped>
 ul {
-    list-style-type: none;
+	list-style-type: none;
 }
+
 li {
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-    margin-bottom: 10px;
-    width: 250px;
+	background-color: white;
+	border-radius: 5px;
+	border: 1px solid rgba(0, 0, 0, 0.2);
+	cursor: pointer;
+	margin-bottom: 10px;
+	width: 250px;
 }
 
 li:hover {
-    background-color: rgba(0, 0, 0, 0.05);
+	background-color: rgba(0, 0, 0, 0.05);
 }
 
 .options-container {
-    display: flex;
-    justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,8 +1,15 @@
 <template>
-  <h1>¿Quién es este pokémon?</h1>
-
-  <PokemonPicture :pokemonId="111" :showPokemon="false"/>
-  <PokemonOptions />
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+    <div v-else>
+        <h1>¿Quién es este pokémon?</h1>
+    
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+        <PokemonOptions :pokemons='pokemonArr' @selection="checkAnswer"/>
+        <template v-if="showAnswer">
+            <h2 class="fade-in">{{message}}</h2>
+            <button @click="newGame">Nuevo juego</button>
+        </template>
+    </div>
 
 </template>
 
@@ -11,16 +18,48 @@ import PokemonPicture from '@/components/PokemonPicture.vue'
 import PokemonOptions from '@/components/PokemonOptions.vue'
 import getPokemonOptions from '@/helpers/getPokemonOptions'
 
-console.log(getPokemonOptions())
+// console.log(getPokemonOptions())
 
 export default {
-    components: { 
-        PokemonPicture,
-        PokemonOptions
+    components: { PokemonPicture, PokemonOptions },
+    data() {
+        return {
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
+            showAnswer: false,
+            message: ''
         }
+    },
+    methods: {
+        async mixPokemonArray() {
+            this.pokemonArr = await getPokemonOptions()
+            const rndInt = Math.floor( Math.random() * 4)
+            this.pokemon = this.pokemonArr[rndInt]
+        },
+        checkAnswer(selectedId){
+            this.showPokemon = true
+            this.showAnswer = true
+            if(selectedId === this.pokemon.id){
+                this.message = `Corecto, tu pokemon es ${this.pokemon.name}`
+            }else{
+                this.message = `Nop, la respuesta correcta es ${this.pokemon.name}`
+            }
+        },
+        newGame(){
+            this.showPokemon = false
+            this.showAnswer = false
+            this.pokemonArr = []
+            this.pokemon = null
+            this.mixPokemonArray()
+        }
+    },
+    mounted() {
+        this.mixPokemonArray()
+    }
 }
 </script>
 
 <style>
-
+    
 </style>
